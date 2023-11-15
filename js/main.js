bbd65050968d0fe64873441a3aac1d770ce9c29d
HodayaGitHub <hodaya1812@gmail.com>
2023-11-15 18:34:58 +0200

    Add increment functionality to onBallClick

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 'use strict'
 
 function onBallClick(elBall) {
-console.log(`This ball was clicked:`)
-console.log(elBall);
+    var elBallHeight = elBall.clientHeight
+    var elBallWidth = elBall.clientWidth
 
+    elBallHeight += 50
+    elBallWidth += 50
+
+    elBall.style.height = elBallHeight + 'px'
+    elBall.style.width = elBallWidth + 'px'
+    elBall.innerText = elBallWidth
 }
