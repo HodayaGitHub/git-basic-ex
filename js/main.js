6d461acb9f60778dcce6554431db53626f01ae99
HodayaGitHub <hodaya1812@gmail.com>
2023-11-15 19:04:24 +0200

    Limit ball size

--- js/main.js
+++ js/main.js
@@ -4,9 +4,14 @@ function onBallClick(elBall) {
     var elBallHeight = elBall.clientHeight
     var elBallWidth = elBall.clientWidth
 
-    elBallHeight += 50
-    elBallWidth += 50
-
+    
+    if (elBallHeight < 400) {
+        elBallHeight += 50
+        elBallWidth += 50
+    } else {
+        elBallHeight = 100
+        elBallWidth = 100
+    }
     elBall.style.height = elBallHeight + 'px'
     elBall.style.width = elBallWidth + 'px'
     elBall.innerText = elBallWidth
