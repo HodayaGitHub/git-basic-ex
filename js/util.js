fa5c094b747a98712adf0bc299d6525c89dfe1f1
HodayaGitHub <hodaya1812@gmail.com>
2023-11-15 20:07:48 +0200

    Implement a random color on each click on the ball

--- js/util.js
+++ js/util.js
@@ -5,3 +5,12 @@ function getRandomInt(min, max) {
     max = Math.floor(max)
     return Math.floor(Math.random() * (max - min) + min)
   }
+
+  function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.round(Math.random() * 15)];
+    }
+    return color;
+}
