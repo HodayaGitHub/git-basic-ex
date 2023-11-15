2f4ebcadf65e05ad98af100ae9ca14c80179061a
HodayaGitHub <hodaya1812@gmail.com>
2023-11-15 22:47:08 +0200

    Before adding functionality to onSixthBallHover

--- js/main.js
+++ js/main.js
@@ -24,20 +24,34 @@ function onBallClick(elBall, maxDiameter) {
     elBall.innerText = elBallsize
 }
 
-
 function onThirdBallClick(elBall) {
-    var firstAndSecondBalls = document.querySelectorAll('.first-ball, .second-ball')
+    var firstBall = document.querySelector('.first-ball')
+    var secondBall = document.querySelector('.second-ball')
 
-    firstAndSecondBalls.forEach(ball => {
-        var sizeToIncrease = 150
-        ball.style.backgroundColor = 'gray'
-        ball.style.height = sizeToIncrease + 'px'
-        ball.style.width = sizeToIncrease + 'px'
-        ball.innerText = sizeToIncrease
+    var firstBallSize = firstBall.clientHeight
+    var secondBallSize = secondBall.clientHeight
 
-    })
+    console.log(firstBallSize)
+    var firstBallColor = window.getComputedStyle(firstBall).backgroundColor
+    var secondBallColor = window.getComputedStyle(secondBall).backgroundColor
+
+
+    firstBall.style.backgroundColor = secondBallColor
+    secondBall.style.backgroundColor = firstBallColor
+
+
+    firstBall.style.width = secondBallSize + 'px'
+    firstBall.style.height = secondBallSize + 'px'
+    firstBall.innerText = secondBallSize
+
+
+    secondBall.style.width = firstBallSize + 'px'
+    secondBall.style.height = firstBallSize + 'px'
+    secondBall.innerText = firstBallSize
+
+    console.log(firstBallColor, secondBallColor)
+
 }
-
 
 function onFourthBallClick(elBall) {
     var firstAndSecondBalls = document.querySelectorAll('.first-ball, .second-ball')
@@ -60,28 +74,47 @@ function onFourthBallClick(elBall) {
         ball.style.height = ballSize + 'px'
         ball.style.width = ballSize + 'px'
         ball.innerText = ballSize
-
     });
 }
-
 
 function onFifthBallClick() {
     document.querySelector('body').style.backgroundColor = getRandomColor()
 }
 
-function onSixthhBallClick() {
+function onSixthBallClick() {
     document.querySelector('body').style.backgroundColor = 'black'
 
-    var elBalls = document.querySelectorAll('.ball')
-    elBalls.forEach(ball => {
+    var firstAndSecondBalls = document.querySelectorAll('.first-ball, .second-ball')
+
+    firstAndSecondBalls.forEach(ball => {
         ball.style.height = '100px'
         ball.style.width = '100px'
-    
-        if (ball === document.querySelector('.first-ball') || ball === document.querySelector('.second-ball')) {
-            ball.innerText = 100
-        }
-
+        ball.innerText = 100
     });
+
+    document.querySelector('.first-ball').style.backgroundColor = 'yellow'
+    document.querySelector('.second-ball').style.backgroundColor = 'rgb(62, 193, 202)'
+}
+
+onSixthBallHover()
+function onSixthBallHover() {
+    var sixthBall = document.querySelector('.sixth-ball')
+    var mouseClickInterval 
+
+    // mouseover - when the mouse is hovering the sixth ball for more than 2 sec: 
+    sixthBall.addEventListener('mouseover', function() {
+        setTimeout(function () {
+            mouseClickInterval = setInterval(console.log('hi'), 2000);
+        }, 2000)
+    })
+
+
+    // mouseout - when the mouse is moving from hovring the sixth ball:
+    sixthBall.addEventListener('mouseout', function() {
+        // Stop the interval when the mouse leaves the sixth ball
+        clearInterval(mouseClickInterval);
+    });
+
 
 
 
