683d8d124b9c7ca60e3d397cc9ea2407c477dc50
HodayaGitHub <hodaya1812@gmail.com>
2023-11-15 19:55:07 +0200

    Increment ball by random amount with limited size

--- js/main.js
+++ js/main.js
@@ -4,14 +4,22 @@ function onBallClick(elBall) {
     var elBallHeight = elBall.clientHeight
     var elBallWidth = elBall.clientWidth
 
-    
+
+    var randomIncrease = getRandomInt(20, 60)
+
     if (elBallHeight < 400) {
-        elBallHeight += 50
-        elBallWidth += 50
+        elBallHeight += randomIncrease
+        elBallWidth += randomIncrease
     } else {
         elBallHeight = 100
         elBallWidth = 100
     }
+
+    if (elBallHeight > 400) {
+        elBallHeight = 400
+        elBallWidth = 400
+    }
+    
     elBall.style.height = elBallHeight + 'px'
     elBall.style.width = elBallWidth + 'px'
     elBall.innerText = elBallWidth
